5ee9cdcf26fccb9b3845962e7ac0be1fbff802cc
CapnAverage <dan@bigredbarrel.com>
2014-01-04 09:33:45 +0000

    Placeholders for custom JQuery

--- header.php
+++ header.php
@@ -20,6 +20,8 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 
+<!-- <script src="/wp-content/themes/brb/js/brb-ui.js" type="text/javascript"></script> --> <!-- Placeholder for custom JQuery-UI -->
+
 <!-- Include Lato font -->
 
 <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,400italic,700italic' rel='stylesheet' type='text/css'>
