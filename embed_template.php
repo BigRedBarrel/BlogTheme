3f5d15263a2831c5bb8c8bcf0f1d3d9977d9a50a
CapnAverage <dan@bigredbarrel.com>
2014-01-06 11:52:56 +0000

    Added Vanilla Forum template, logos, placeholder for custom JS.

---
+++ embed_template.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Vanilla Forum Template
+ *
+ * A custom page template for a Vanilla Forum.
+ *
+ * The "Template Name:" bit above allows this to be selectable
+ * from a dropdown menu on the edit page screen.
+ *
+ */
+
+get_header();
+the_content();
+
+?>
+
+<script type="text/javascript" src="http://localhost:8888/vanilla/js/embed.js"></script>
+<noscript>Please enable JavaScript to view the <a href="http://vanillaforums.com/?ref_noscript">discussions powered by Vanilla.</a></noscript>
+<div class="vanilla-credit"><a class="vanilla-anchor" href="http://vanillaforums.com">Discussions by <span class="vanilla-logo">Vanilla</span></a></div>
+
+<?php 
+
+get_footer();
